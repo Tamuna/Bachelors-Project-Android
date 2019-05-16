ecfff6178f9948ebbf4a94067d2abbc495d9f8c2
tgeldiashvili <tgeldiashvili@bog.ge>
2019-05-16 13:13:39 +0400

    cmt

---
+++ 2/app/src/main/java/ge/edu/freeuni/rsr/FinishActivity.java
@@ -0,0 +1,14 @@
+package ge.edu.freeuni.rsr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FinishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finish);
+    }
+}
